de2e2f1790662c1a9d09b16a8881bab223952163
Frank Christiaan Peelen <frank.peelen@yahoo.com>
2015-05-17 13:14:46 +0900

    Set up basics

---
+++ javascript.js
@@ -0,0 +1,15 @@
+var add = function (num1, num2) {
+	return num1 + num2;
+};
+
+var multiply = function (num1, num2) {
+	return num1 * num2;
+};
+
+var divide = function(num1, num2) {
+	return num1 / num2;
+};
+
+var substract = function(num1, num2) {
+	return num1 - num2;
+};
